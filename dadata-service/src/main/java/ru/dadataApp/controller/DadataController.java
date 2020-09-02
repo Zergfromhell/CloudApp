722330120d74d48a522e46cacba6619ad6ff625c
Zergfromhell <kalovey2@gmail.com>
2020-09-02 22:32:47 +0400

    Minor changes

--- dadata-service/src/main/java/ru/dadataApp/controller/DadataController.java
+++ dadata-service/src/main/java/ru/dadataApp/controller/DadataController.java
@@ -1,23 +1,15 @@
 package ru.dadataApp.controller;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.dadataApp.service.DadataService;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
-
-@Controller
+@RestController
 @RequestMapping("/dadata")
 public class DadataController {
     DadataService dadataService;
@@ -28,11 +20,10 @@ public class DadataController {
     }
 
     @GetMapping("/search/{id}")
-    public String cleanAddress(@PathVariable(name = "id", required = false) String id, Model model) throws IOException {
+    public String cleanAddress(@PathVariable(name = "id") String id) throws IOException {
 
         String resultRequest = dadataService.displayReturnedRequest("https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/" + id);
 
-        model.addAttribute("resultString", resultRequest);
-        return "result";
+        return resultRequest;
     }
 }
--- database-service/src/main/java/ru/databaseApp/controller/CityController.java
+++ database-service/src/main/java/ru/databaseApp/controller/CityController.java
@@ -1,12 +1,10 @@
 package ru.databaseApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.databaseApp.repository.CityRepository;
 
-@Controller
+@RestController
 @RequestMapping("/database")
 public class CityController {
     private CityRepository cityRepository;
@@ -17,9 +15,8 @@ public class CityController {
     }
 
     @GetMapping("/search/{id}")
-    public String autoName(@PathVariable(name = "id", required = false) String id, Model model) {
-        model.addAttribute("resultString", cityRepository.getCityByKladr_id(id).toString());
+    public String autoName(@PathVariable(name = "id") String id) {
 
-        return "resultDB";
+        return cityRepository.getCityByKladr_id(id).toString();
     }
 }
